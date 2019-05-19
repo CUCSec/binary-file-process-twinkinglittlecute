3613ea46cdb8f3a05783d6d135c183f27e79d283
twinkinglittlecute <864364682@qq.com>
2019-05-19 13:06:45 +0800

    安老师，我感觉你的detect函数跟我那个跳转修改不匹配，然后我就把那个函数改了，或者我会错题意了

--- Conan.py
+++ Conan.py
@@ -1,24 +1,42 @@
 import struct
-
+import string 
 
 def tamper(student_id):
-  pass
+  with open('lenna.bmp','r+b') as f:
+    list=[]
+    student_id=int(student_id)
+    f.seek(54)
+    for i in range(12):
+      list.append(student_id%10)
+      student_id=int(student_id/10)
+    list.reverse()
+    for j in range(12):
+      step=list[j]
+      print(step)
+      if step==0:
+        f.seek(10,1)
+        f.write(b'\x00\x00\x00')
+     
+      else:
+        f.seek(step,1)
+        f.write(b'\x00\x00\x00')
+     
+      
+      
+    
+
 
 
 def detect():
   with open('lenna.bmp', 'rb') as f:
-    bmp_file_header = f.read(14)
-
-    bm, size, r1, r2, offset = struct.unpack('<2sIHHI', bmp_file_header)
-
-    f.seek(offset)
-
+    f.seek(54)
     count = 12
     offset = 0
     last_offset = 0
     while count > 0:
       color = f.read(3)
-
+      if color!=b'\x00\x00\x00':
+        f.seek(-2,1)
       if color == b'\x00\x00\x00':
 
         if offset - last_offset == 10:
@@ -26,10 +44,12 @@ def detect():
         else:
           print(offset - last_offset)
 
-        last_offset = offset
+        last_offset = offset+1
         count -= 1
 
       offset += 1
+
+      
 
 
 if __name__ == '__main__':
